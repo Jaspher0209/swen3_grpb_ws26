fcffb7cda48b9b46f5cf7665b24a5220d2642fca
jaspher <if24b110@technikum-wien.at>
2026-09-15 18:09:43 +0200

    Created Models - Documents - Metadata - Message

---
+++ PaperlessREST/Models/DocumentMetaData.cs
@@ -0,0 +1,9 @@
+﻿namespace Models;
+
+public class DocumentMetaData
+{
+    public string filename;
+    public string description;
+    public string updated;
+    public string author;
+}
---
+++ PaperlessREST/Models/Message.cs
@@ -0,0 +1,6 @@
+namespace Models;
+
+public class Message
+{
+    public string message;
+}
