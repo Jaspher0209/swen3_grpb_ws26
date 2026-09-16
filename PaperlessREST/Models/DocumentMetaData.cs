9710b76e2185c67221ab4eca3da0a6ea7b568af2
jaspher <if24b110@technikum-wien.at>
2026-09-16 10:43:59 +0200

    Modified Models

--- PaperlessREST/Models/DocumentMetaData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Models;
-
-public class DocumentMetaData
-{
-    public string filename;
-    public string description;
-    public string updated;
-    public string author;
-}
---
+++ PaperlessREST/Models/MetaData.cs
@@ -0,0 +1,10 @@
+﻿namespace Models;
+
+public class MetaData
+{
+    public int id { get; set; }
+    public string filename { get; set; }
+    public string description { get; set; }
+    public string updated { get; set; }
+    public string author { get; set; }
+}
