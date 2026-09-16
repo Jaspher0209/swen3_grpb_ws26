042ae54b44017cf5c42ca6e375cf6075f317ef1b
Suhail-Edipoglu <suhail.edipoglu@gmail.com>
2026-09-16 10:45:01 +0200

    dal project added

---
+++ PaperlessREST/PaperlessREST.Bll/DocumentService.cs
@@ -0,0 +1,11 @@
+using PaperlessREST.Models;
+
+namespace PaperlessREST.Bll;
+
+public class DocumentService : IDocumentService
+{
+    public Task<Document> GetDocumentByIdAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+}
