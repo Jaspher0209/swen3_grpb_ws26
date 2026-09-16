64304c5602569680c21f94da36cef80bd84722f7
jaspher <if24b110@technikum-wien.at>
2026-09-16 10:44:42 +0200

    Modified Repository

--- PaperlessREST/Dal/DocumentMetaRepository.cs
+++ PaperlessREST/Dal/DocumentMetaRepository.cs
@@ -1,32 +1,54 @@
-﻿using Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
 
 namespace Dal;
 
 public class DocumentMetaRepository : IRepository
 {
-    // Retrieve Document from Db
-    public Document GetDocument(int id)
+    private readonly MetadataDbContext _dbContext;
+
+    public DocumentMetaRepository(MetadataDbContext dbContext)
     {
-        throw new NotImplementedException();
+        _dbContext = dbContext; 
+    }
+    
+    
+    // Retrieve Document from Db
+    public async Task<MetaData?> GetDocument(int id)
+    {
+        var products = await _dbContext.MetaData.FirstOrDefaultAsync();
+        return products;
     }
 
     // Upload Document into Db
-    public int UploadDocument(Document metaData)
+
+    public async Task<int> UploadDocument(MetaData metaData)
     {
-        throw new NotImplementedException();
+        _dbContext.MetaData.Add(metaData);
+        await _dbContext.SaveChangesAsync();
+        return metaData.id;
     }
 
     // Edit Document in Db
-    
-    public bool EditDocument(int id, Document metaData)
+    public async Task EditDocument(int id, MetaData metaData)
     {
-        throw new NotImplementedException();
+        var found = await _dbContext.MetaData.FindAsync(id);
+        
+        if (found != null)
+        {
+            found = metaData;
+            await _dbContext.SaveChangesAsync(); // Auto-detects and applies changes!
+        }
     }
 
     // Remove Document from Db
-    
-    public bool RemoveDocument(int id)
+    public async Task RemoveDocument(int id)
     {
-        throw new NotImplementedException();
+        var found = await _dbContext.MetaData.FindAsync(id);
+        if (found != null)
+        {
+            _dbContext.MetaData.Remove(found);
+            await _dbContext.SaveChangesAsync();
+        }
     }
 }
