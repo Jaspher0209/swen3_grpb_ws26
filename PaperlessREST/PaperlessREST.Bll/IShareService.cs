d90c9e25a65ca7b01ef6d63c5254a4d495432fcc
Janybanny <git@janybanny.moe>
2026-09-22 11:53:51 +0200

    Add Share Service Interface

---
+++ PaperlessREST/PaperlessREST.Bll/IShareService.cs
@@ -0,0 +1,9 @@
+using Models;
+
+namespace PaperlessREST.Bll;
+
+public interface IShareService {
+    Task<string> CreateShareLinkAsync(string link, string password, DateTime expirationDate);
+    Task<MetaData> ResolveDocumentMetadataFromLinkAsync(string id, string password);
+    Task<Document> ResolveDocumentContentFromLinkAsync(string id, string password);
+}
